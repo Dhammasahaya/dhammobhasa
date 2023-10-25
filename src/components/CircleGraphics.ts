360d48fd3b000128a9022b0cecd30a1e6a397eed
Jordi Yaputra <jordiyaputra@gmail.com>
2023-10-26 05:27:31 +0700

    feat citta render

--- src/components/CircleGraphics.ts
+++ src/components/CircleGraphics.ts
@@ -8,7 +8,11 @@ export interface CircleGraphicsConfig {
 
 export class CircleGraphics extends Phaser.GameObjects.Graphics {
   _radius: number;
-  _stroke: number = 16777215;
+  _stroke: number = 0xffffff;
+  _strokeAlpha: number = 0;
+  _strokeThickness: number = 2;
+  _fill: number = 0xffffff;
+  _fillAlpha: number = 0;
 
   constructor(
     scene: Phaser.Scene,
@@ -19,11 +23,22 @@ export class CircleGraphics extends Phaser.GameObjects.Graphics {
   ) {
     super(scene, { x, y });
     this._radius = radius;
+    this._fill = config.fill ?? this._fill;
     this._stroke = config.stroke ?? this._stroke;
+    this._fillAlpha = config.fillAlpha ?? this._fillAlpha;
+    this._strokeThickness = config.strokeThickness ?? this._strokeThickness;
+    this._strokeAlpha =
+      config.strokeAlpha ?? config.stroke !== undefined ? 1 : this._strokeAlpha;
+
+    this.updateState();
   }
 
   updateState() {
-    this.lineStyle(2, this._stroke);
+    this.clear()
+    this.lineStyle(2, this._stroke, this._strokeAlpha);
     this.strokeCircle(0, 0, this._radius);
+
+    this.fillStyle(this._fill, this._fillAlpha);
+    this.fillCircle(0, 0, this._radius);
   }
 }
--- src/components/CittaComponent.ts
+++ src/components/CittaComponent.ts
@@ -1,11 +1,27 @@
 import { Citta } from "../entities/Citta";
 import { VedanaType } from "../entities/types";
+import { CircleGraphicsConfig } from "./CircleGraphics";
 import { NamaComponent, NamaComponentFromEntityOption } from "./NamaComponent";
 
 export class CittaComponent extends NamaComponent {
   _numOfHetu: number = 0;
+  _hetuRenderFactor: number = 0.2;
   sahetuka: boolean = false;
   vedana: VedanaType = "upekkha";
+
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    radius: number,
+    config: CircleGraphicsConfig = {}
+  ) {
+    super(scene, x, y, radius, {
+      ...config,
+      strokeAlpha: config.strokeAlpha ?? 1,
+    });
+    super.updateState();
+  }
 
   setHetu(num: number): void {
     this._numOfHetu = num;
@@ -15,15 +31,7 @@ export class CittaComponent extends NamaComponent {
 
   setVedana(vedana: VedanaType) {
     this.vedana = vedana;
-    this.updateRenderState();
-  }
 
-  updateRenderState() {
-    // update hetu render
-    this.fillStyle(16777215);
-    this.fillCircle(0, 0, this._radius * 0.2);
-
-    // update vedana render
     switch (this.vedana) {
       case "somanassa":
         this._stroke = 16776960;
@@ -31,8 +39,57 @@ export class CittaComponent extends NamaComponent {
       case "domanassa":
         this._stroke = 16711680;
         break;
+      case "upekkha":
+        this._stroke = 0xaaaaaa;
+        break;
+      case "dukkha":
+        this._stroke = 0xaa00ff;
+        break;
+      case "sukha":
+        this._stroke = 0xff8800;
+        break;
     }
+
+    this.updateRenderState();
+  }
+
+  updateRenderState() {
     super.updateState();
+
+    // update hetu render
+    if (this._numOfHetu) {
+      this.fillStyle(0xffffff);
+      if (this._numOfHetu === 1) {
+        this.fillCircle(0, 0, this._radius * this._hetuRenderFactor);
+      } else if (this._numOfHetu === 2) {
+        this.fillCircle(
+          0,
+          -this._radius / 3,
+          this._radius * this._hetuRenderFactor
+        );
+        this.fillCircle(
+          0,
+          this._radius / 3,
+          this._radius * this._hetuRenderFactor
+        );
+      } else if (this._numOfHetu === 3) {
+        this.fillCircle(
+          0,
+          -this._radius / 3,
+          this._radius * this._hetuRenderFactor
+        );
+        this.fillCircle(
+          -this._radius / 3,
+          this._radius / 4,
+          this._radius * this._hetuRenderFactor
+        );
+        this.fillCircle(
+          this._radius / 3,
+          this._radius / 4,
+          this._radius * this._hetuRenderFactor
+        );
+      }
+    }
   }
 
   static fromEntity(
